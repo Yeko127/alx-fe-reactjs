0f5b967a0a2a775e3301618a2ff2dcd90a1c8e12
Carolyne Yeko <yekocarol2@gmail.com>
2026-01-10 13:42:19 +0300

    Added the UserProfile component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,6 +8,7 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/header';
 import MainContent from './components/MainContent';
 import Footer from './components/footer';
+import UserProfile from './components/UserProfile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +16,7 @@ function App() {
   return (
     <>
       <WelcomeMessage />
+      <UserProfile name = "Alice" age = {25}  bio = "Loves hiking and photography"/>
       <Header />
       <MainContent />
       <Footer />
