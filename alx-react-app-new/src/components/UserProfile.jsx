e1f20cb05d5943f73c981231aa46e4c73f4beceb
Carolyne Yeko <yekocarol2@gmail.com>
2026-01-15 08:49:24 +0300

    Added the the word solid blue on the styling of userprofile

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
  const UserProfile = (props) => {
    return (
      <div style={{border: '1px white', padding: '10px', margin: '10px', backgroundColor: 'grey'}}>
-       <h2 style={{color: 'black', fontWeight: 'semi-bold'}} >{props.name}</h2>
+       <h2 style={{color: 'solid blue', fontWeight: 'semi-bold'}} >{props.name}</h2>
        <p>Age: <span style={{fontWeight: 'bold'}}> {props.age} </span></p>
        <p>Bio: {props.bio}</p>
      </div>
