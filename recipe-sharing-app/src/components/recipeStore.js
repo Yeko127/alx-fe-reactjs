8e67efc9533efe12739d60348a5c9047b68f1559
Carolyne Yeko <yekocarol2@gmail.com>
2026-01-25 10:29:36 +0300

    set the get on zustand store

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,9 +1,11 @@
 import { create } from 'zustand'
 
-const useRecipeStore = create(set => ({
+const useRecipeStore = create((set, get => ({
   recipes: [],
   searchTerm: '',
   favorites: [],
+  recommendations: [],
+
 
   setSearchTerm: (term) => set({ searchTerm: term}),
 
@@ -18,29 +20,31 @@ const useRecipeStore = create(set => ({
     );
   },
 
-  addFavorite: (recipeId) => set(state => ({favorites: state.favorites.includes(recipeId) ? state.favorites: [...state.favorites, recipeId],})),
-  removeFavorite: (recipeId) => set(state => ({favorites: state.favorites.filter(id => id !== recipeId),})),
-  isFavorite: (recipeId) => get().favorites.includes(recipeId),
+  addFavorite: (recipeId) => 
+    set(state => ({
+      favorites: state.favorites.includes(recipeId) 
+      ? state.favorites
+      : [...state.favorites, recipeId],
+    })),
 
-  recommendations: () => {
-    const {recipes, favorites} = get();
-    if (favorites.length ===0) return [];
+  removeFavorite: (recipeId) => 
+    set(state => ({
+      favorites: state.favorites.filter(id => id !== recipeId),
+    })),
 
-    const favoriteRecipes = recipes.filter(r => favorites.includes(r.id));
 
-    return recipes.filter(recipe => {
-      if (favorites.includes(recipe.id)) return false;
-
-      return favoriteRecipes.some(fav =>
-        fav.category && recipe.category === fav.category
-      )
-    });
-  },
+  generateRecommendations: () => set(state => {
+    const recommended = state.recipes.filter(recipe =>
+      state.favorites.includes(recipe.id) && Math.random() > 0.5
+    );
+    return {recommendations: recommended};
+  }),
   
 
   addRecipe: (recipe) => set(state => ({ recipes: [...state.recipes, recipe] })),
   deleteRecipe: (id) => set(state => ({ recipes: state.recipes.filter((recipe) => recipe.id !==id) })),
   updateRecipe: (updatedRecipe) => set(state => ({ recipes: state.recipes.map((recipe) => recipe.id === updatedRecipe.id ? updatedRecipe : recipe), })),
+  
   setRecipes: (recipes) => set({ recipes })
 
   
