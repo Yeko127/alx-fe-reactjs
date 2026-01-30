60ec311c8362420781478ced51ada0d59bd33fb8
Carolyne Yeko <yekocarol2@gmail.com>
2026-01-30 11:12:49 +0300

    added the baseURL

---
+++ github-user-search/src/services/GitHubAPI.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const GitHubAPI = axios.create ({baseURL: "https://api.github.com"});
+
+export default GitHubAPI;
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const GITHUB_API_URL = "https://api.github.com/users";
+//const GITHUB_API_URL = "https://api.github.com/users";
 
 export const fetchUserData = async (username) => {
-    const response = await axios.get(`${GITHUB_API_URL}/${username}`);
+    const response = await axios.get(`https://api.github.com/users/${username}`);
     return response.data;
 };
 
