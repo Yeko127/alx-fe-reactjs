ec078eb20b9eb25bece5699e932cc7b6ddd8fd4d
Carolyne Yeko <yekocarol2@gmail.com>
2026-02-01 10:42:26 +0300

    added the right URL on github services

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com";
+const BASE_URL = "https://api.github.com/search/users?q";
 
 // BASIC USER FETCH
 export const fetchUserData = async (username) => {
