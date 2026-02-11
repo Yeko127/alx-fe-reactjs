4dcd923f24c3a2ea5769cd383907fe13b00c92aa
Carolyne Yeko <yekocarol2@gmail.com>
2026-02-11 09:31:10 +0300

    Included the responsive design

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -13,7 +13,7 @@ const HomePage = () => {
                 Recipe Collection
             </h1>
 
-            <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            <div className="grid gap-8 sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
                 {recipes.map((recipe) => (
                     <div
                         key={recipe.id}
