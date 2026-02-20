980017859d2938515e06138385c47841ab4ac8d5
Carolyne Yeko <yekocarol2@gmail.com>
2026-02-20 11:39:03 +0300

    Included string().required of formikForm

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,18 +1,15 @@
 import { Formik, Form, Field, ErrorMessage } from "formik"
-import *as Yup from "yup";
+import * as Yup from "yup";
 
 const formikForm= () => {
     const validationSchema = Yup.object({
-       username: Yup.string()
-       .required("Username is required"),
+       username: Yup.string().required("Username is required"),
 
        email: Yup.string()
-       .email("Invalid email format")
-       .required("Email is required"),
+       .email("Invalid email format").required("Email is required"),
 
        password: Yup.string()
-       .min(6, "Password must be at least 6 characters")
-       .required("Password is required")
+       .min(6, "Password must be at least 6 characters").required("Password is required")
     });
 
     return (
